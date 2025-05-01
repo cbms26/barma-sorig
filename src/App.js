4592731e8d3898d3101b80446a9377b436dcb3b9
Ngawang Tenzin <ngawang.tenzin1@my.jcu.edu.au>
2025-05-01 11:15:56 +1000

    Add routing and header component with navigation links
    - Integrated react-router-dom for client-side routing.
    - Updated App.js to include routes for Home, About, Services, Gallery, and Contact pages.
    - Enhanced Header component with navigation links and company logo.
    - Changed document title to "Barma Sorig Healing Centre".
    - Added global styles in App.css for better layout and design.
    - Included logo image in the project.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Service from "./pages/Services";
@@ -8,15 +10,19 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <Home />
-      <About />
-      <Service />
-      <Contact />
-      <Gallery />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,35 @@
+import { Link } from "react-router-dom";
+import logo from "../logo.png";
+import "../App.css";
+
 function Header() {
-  return <h1>This is header section</h1>;
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-left ">
+          <img src={logo} alt="company logo"></img>
+          <div className="header-title">Barma Sorig</div>
+        </div>
+
+        <nav className="header-nav">
+          <ul className="header-nav-list">
+            <li className="header-nav-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="header-nav-item">
+              <Link to="/About">About Us</Link>
+            </li>
+            <li className="header-nav-item">
+              <Link to="/Services">Services</Link>
+            </li>
+            <li className="header-nav-item">
+              <Link to="/Gallery">Gallery</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
